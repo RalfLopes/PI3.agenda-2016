2c3cd01c0086b712fee373342ba11abbb61a71e9
temp.cas <temp.cas@CAS419659W07I06.senacsp.edu.br>
2016-03-01 18:38:54 -0300

    subindo estrutura plSQL inserção

--- Agenda/src/main/java/br/senac/tads/pi3/blacksytem/agenda/Contato.java
+++ Agenda/src/main/java/br/senac/tads/pi3/blacksytem/agenda/Contato.java
@@ -11,6 +11,50 @@ package br.senac.tads.pi3.blacksytem.agenda;
  */
 public class Contato {
     
+    private String NM_PESSOA;
+    private String DT_NASCIMENTO;
+    private String TELEFONE;
+    private String VL_EMAIL;
+
+    public Contato(String NM_PESSOA, String DT_NASCIMENTO, String TELEFONE, String VL_EMAIL) {
+        this.NM_PESSOA = NM_PESSOA;
+        this.DT_NASCIMENTO = DT_NASCIMENTO;
+        this.TELEFONE = TELEFONE;
+        this.VL_EMAIL = VL_EMAIL;
+    }
+
+    public String getNM_PESSOA() {
+        return NM_PESSOA;
+    }
+
+    public void setNM_PESSOA(String NM_PESSOA) {
+        this.NM_PESSOA = NM_PESSOA;
+    }
+
+    public String getDT_NASCIMENTO() {
+        return DT_NASCIMENTO;
+    }
+
+    public void setDT_NASCIMENTO(String DT_NASCIMENTO) {
+        this.DT_NASCIMENTO = DT_NASCIMENTO;
+    }
+
+    public String getTELEFONE() {
+        return TELEFONE;
+    }
+
+    public void setTELEFONE(String TELEFONE) {
+        this.TELEFONE = TELEFONE;
+    }
+
+    public String getVL_EMAIL() {
+        return VL_EMAIL;
+    }
+
+    public void setVL_EMAIL(String VL_EMAIL) {
+        this.VL_EMAIL = VL_EMAIL;
+    }
+    
     
     
 }
