6f25ab00af15715a920a379f079824f85a52f82c
Isaquebc <>
2016-03-01 19:43:19 -0300

    Adicionado o Insert ao projeto.

--- Agenda/src/main/java/br/senac/tads/pi3/blacksytem/agenda/Agenda.java
+++ Agenda/src/main/java/br/senac/tads/pi3/blacksytem/agenda/Agenda.java
@@ -7,13 +7,16 @@ package br.senac.tads.pi3.blacksytem.agenda;
   import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -21,14 +24,7 @@ import java.util.logging.Logger;
  */
 public class Agenda {
     
-    
-    
-    
-    
-    
-  
-
-
+ static Scanner leia = new Scanner(System.in); 
 
     private Connection obterConexao() throws SQLException, ClassNotFoundException {
         Connection conn = null;
@@ -84,6 +80,50 @@ public class Agenda {
                 }
             }
         }
+    }
+    
+    public void inserirPessoas(){
+        
+        Statement stmt = null;
+        PreparedStatement stm = null;
+        Connection conn = null;
+        
+        System.out.println("Insira nome do contato");
+        String nome = leia.next();
+        System.out.println("Insira Data de Nascimento");
+        String data = leia.next();
+        System.out.println("Insira Telefone");
+        String telefone = leia.next();
+        System.out.println("Insira E-mail");
+        String email = leia.next();
+        
+        Contato contato = new Contato(nome, data, telefone, email);
+        
+        try {
+            String sql = "INSERT INTO TB_PESSOA ( NM_PESSOA, DT_NASCIMENTO, VL_TELEFONE, VL_EMAIL), values(?,?,?,?)";
+            conn= obterConexao();
+        
+            stm = conn.prepareStatement(sql);
+            stm.setString(1, contato.getNM_PESSOA());
+            stm.setString(2, contato.getDT_NASCIMENTO());
+            stm.setString(3, contato.getTELEFONE());
+            stm.setString(4, contato.getVL_EMAIL());    
+            
+            stm.executeUpdate();
+            System.out.println("Cadastro realizado com Sucesso");
+            JOptionPane.showMessageDialog(null, "Cadastro realizado com Sucesso");
+            stm.close();
+            conn.close();
+            
+            
+        } catch (SQLException ex) {
+            System.out.println("Erro no cadastro.");
+            System.out.println(ex.getMessage());
+            
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Dado não inserido.");
+        }
+        
     }
 
 }
